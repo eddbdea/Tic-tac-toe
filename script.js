ca2a93cb50590bf15ae6f14f7a786766a50795d3
Eduard Badea <eduardbadea@outlook.com>
2025-02-11 19:14:10 +0200

    Update codebase
    Added a CSS file and modified script.js file.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const MAX_ELEMENTS = 8;
 const MAX_LENGTH = 9;
-let playerStatus = 1;
-let gameValues = Array(MAX_LENGTH).fill(null);
-let winConditions = [
+let curentPlayer = 1;
+let emptySpotsLeft = MAX_LENGTH;
+const gameValues = Array(MAX_LENGTH).fill(null);
+const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -13,49 +14,66 @@ let winConditions = [
     [2, 4, 6]
 ];
 
+function createBoardGame() {
+    for (let i = 0; i < MAX_LENGTH; ++i) {
+        const divElem = document.createElement('div');
+        document.getElementById('main-div').appendChild(divElem);
+        divElem.classList.add('col', 'board-style');
+        divElem.id = 'box' + i;
+        divElem.onclick = () => {
+            gameStatus(divElem);
+        }
+    }
+}
+
+createBoardGame();
+
 function gameStatus(div) {
     const currentElement = div.innerHTML;
     const playerTurn = document.getElementById('player-move');
     const index = parseInt(div.id.replace('box', '')); 
     addGameSign(index, currentElement, div);
-    let win = checkWin(gameValues, winConditions);
-    gameWinStatus(win, playerTurn, index);
+    const win = checkWin(gameValues, winConditions);
+    gameWinStatus(win, playerTurn);
 }
 
 function checkWin(gameValues, winConditions) {
     return winConditions.some(winner => {
-        let a = winner[0];
-        let b = winner[1];
-        let c = winner[2];
-        return gameValues[a] && gameValues[a] === gameValues[b] && gameValues[a] === gameValues[c];
+        const a = winner[0];
+        const b = winner[1];
+        const c = winner[2];
+        return gameValues[a] && gameValues[a] === gameValues[b] 
+        && gameValues[a] === gameValues[c];
     });
 }
 
 function addGameSign(index, currentElement, boxElement) {
-    if (currentElement === '' && playerStatus === 1) {
+    if (currentElement === '' && curentPlayer === 1) {
         boxElement.innerHTML = 'X';
         gameValues[index] = 'X';
-    } else if (currentElement === '' && playerStatus === 2) {
+    } else {
         boxElement.innerHTML = 'O';
         gameValues[index] = 'O';
     }
+    --emptySpotsLeft;
 }
 
-function gameWinStatus(win, player, index) {
+function gameWinStatus(win, player) {
     if (win) {
-        player.innerHTML = 'Player ' + playerStatus + ' wins!';
         document.getElementById('restart').removeAttribute('hidden');
-    } else if (index === MAX_ELEMENTS) {
-        player.innerHTML = 'It`s a draw!';
+        emptySpotsLeft = MAX_LENGTH;
+        return player.innerHTML = 'Player ' + curentPlayer + ' wins!';
+    } else if (emptySpotsLeft === 0) {
         document.getElementById('restart').removeAttribute('hidden');
-    } else {
-        if (playerStatus === 1) {
-            playerStatus = 2;
-        } else if (playerStatus === 2) {
-            playerStatus = 1;
-        }
-        player.innerHTML = 'Player move: player ' + playerStatus;
+        emptySpotsLeft = MAX_LENGTH;
+        return player.innerHTML = 'It`s a draw!';
     }
+    if (curentPlayer === 1) {
+        curentPlayer = 2;
+    } else {
+        curentPlayer = 1;
+    }
+    return player.innerHTML = 'Player move: player ' + curentPlayer;
 }
 
 function restartGame() {
